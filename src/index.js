0f85b87a0964886398a9a9c49d785a447f526c4e
series2gether <seriestogetherapp@gmail.com>
2022-12-28 15:23:26 -0300

    Added: Several new implementations

--- src/index.js
+++ src/index.js
@@ -63,6 +63,7 @@ app.use(require('./routes/authentication'));
 app.use('/links',require('./routes/links'));
 app.use('/user', require('./routes/user'));
 app.use('/home', require('./routes/series'));
+app.use('/streaming', require('./routes/streaming'));
 
 //Public
 app.use(express.static(path.join(__dirname, 'public')));
--- src/routes/series.js
+++ src/routes/series.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 var expressHbs =  require('express-handlebars');
+const { isLoggedIn } = require('../lib/auth');
 
 var hbs = expressHbs.create({});
 
@@ -11,7 +12,7 @@ const pool = require('../database');
 router.get('/', async (req, res) => {
     
     const series = await pool.query('SELECT * FROM series');
-    res.render('home/series', {series: series});
+    res.render('home/series', {series: series, style: 'style.css'});
     console.log(series);
 });
 
@@ -19,7 +20,42 @@ router.get('/series/delete/:id', async (req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM series WHERE id = ?', [id]);
     res.redirect('/home');
-})
+});
+
+router.get('/series/:id', async (req, res) => {
+    const { id } = req.params;
+    const series = await pool.query('SELECT * FROM series WHERE id = ?', [id]);
+    const commentaries = await pool.query('SELECT * FROM commentaries WHERE serieId = ?', [id]);
+    console.log('series > ', series);
+    console.log('series > ', series[0]);
+    console.log('commentaries >> ', commentaries);
+    const counter = commentaries.length;
+    res.render('series/details', {series: series[0], commentaries: commentaries, counter: counter, style: 'details.css'});
+});
+
+router.get('/addCommentary', (req, res) => {
+    res.render('series/addCommentary');
+});
+
+router.post('/series/addCommentary/:id', isLoggedIn, async (req, res) => {
+    console.log('req.user >>> ', req.user);
+    const userId = req.user.id;
+    const { username } = req.user;
+    console.log('req.params >>> ', req.params);
+    const { id } = req.params;
+    console.log('id param >>>', id);
+    const { description } = req.body;
+    console.log('description from form >> ', description);
+    const newCommentary = {
+        serieId: id,
+        idUsuarios: userId,
+        description: description,
+        username: username
+    };
+    const commentary = await pool.query('INSERT INTO commentaries set ?',[newCommentary]);
+    req.flash('success', 'Commentary added successfully!');
+    res.redirect('/home/series/'+id);
+});
 
 hbs.handlebars.registerHelper('clickExample', function () {
     console.log('clicked');
