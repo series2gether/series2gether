0835b7d90c34781bff8229862058d72469728b66
series2gether <seriestogetherapp@gmail.com>
2023-04-08 19:36:18 -0300

    Added cors and proxy

--- src/routes/watchRoom.js
+++ src/routes/watchRoom.js
@@ -3,6 +3,9 @@ const router = express.Router();
 var expressHbs =  require('express-handlebars');
 const { isLoggedIn, isAdmin } = require('../lib/auth');
 const url = require('url');
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({});
+const cors = require('cors');
 
 var hbs = expressHbs.create({});
 
@@ -11,7 +14,22 @@ const pool = require('../database');
 const { log } = require('console');
 
 router.get('/', isLoggedIn, isAdmin, async (req, res) => {
+    
     let videoURL = req.query.videoSrc;
+    const options = {
+        target: "https://delivery337.akamai-video-content.com", // Proxy target
+        port: 5050, // Proxy server listening port
+    };
+    // Add the CORS headers to the response
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Proxy the request to the target server
+    proxy.web(req, res, options, function (e) {
+        console.log(e);
+    });
+
     res.render('watchRoom/watchRoom', {style: 'style.css', videoURL: videoURL});
 });
 
