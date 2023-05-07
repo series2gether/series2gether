986b6abcf4b52547af6e71c0bb9f049bf4995a7a
series2gether <seriestogetherapp@gmail.com>
2023-05-07 01:50:30 -0300

    Added proxy server

--- src/routes/watchRoom.js
+++ src/routes/watchRoom.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({});
 var expressHbs =  require('express-handlebars');
 const { isLoggedIn, isAdmin } = require('../lib/auth');
 const url = require('url');
@@ -13,6 +15,20 @@ const { log } = require('console');
 
 router.get('/', isLoggedIn, isAdmin, async (req, res) => {
     let videoURL = ''//req.query.videoSrc;
+
+    const options = {
+        target: "https://mega99.uqload.co", // Proxy target
+        port: 5050, // Proxy server listening port
+    };
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Proxy the request to the target server
+    proxy.web(req, res, options, function (e) {
+        console.log(e);
+    });
     res.render('watchRoom/watchRoom', {style: 'style.css', videoURL: videoURL});
 });
 
