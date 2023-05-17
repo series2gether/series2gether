915aa6696ce4992def695b27be3ba86714a21adb
series2gether <seriestogetherapp@gmail.com>
2023-05-16 23:00:53 -0300

    Added new m3u8 detection

--- src/routes/watchRoom.js
+++ src/routes/watchRoom.js
@@ -1,37 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer({});
-var expressHbs =  require('express-handlebars');
 const { isLoggedIn, isAdmin } = require('../lib/auth');
-const url = require('url');
-const cors = require('cors');
-
-var hbs = expressHbs.create({});
-
-//Llamo para conectar la base
-const pool = require('../database');
-const { log } = require('console');
 
 router.get('/', isLoggedIn, isAdmin, async (req, res) => {
-    let videoURL = ''//req.query.videoSrc;
 
-    const options = {
-        target: "https://mega99.uqload.co", // Proxy target
-        port: 5050, // Proxy server listening port
-    };
+	const urlToPass = req.originalUrl;
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	const videoURL = urlToPass.replace('/watchRoom?videoSrc=', '');
 
-    // Proxy the request to the target server
-    proxy.web(req, res, options, function (e) {
-        console.log(e);
-        res.render('watchRoom/watchRoom', {style: 'style.css', videoURL: videoURL});
-    });
-    
+	res.render('watchRoom/watchRoom', { style: 'style.css', videoURL: videoURL });
+
 });
-
 
 module.exports = router;
